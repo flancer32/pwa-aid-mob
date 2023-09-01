18555ed0b7489c5cb95c6d863263bceea1bb4ab7
Alex Gusev <alex@flancer64.com>
2023-09-01 11:50:39 +0300

    WF-671 Move TeqFw_Di_Container_A_Parser_Chunk_Old out from the @teqfw/di

--- src/Front/App.mjs
+++ src/Front/App.mjs
@@ -123,9 +123,11 @@ export default class Aid_Mob_Front_App {
                 //
                 router.afterEach((to) => {
                     const _paq = window._paq;
-                    _paq.push(['setCustomUrl', to.path]);
-                    _paq.push(['setDocumentTitle', document.title]);
-                    _paq.push(['trackPageView']);
+                    if (_paq?.push) {
+                        _paq.push(['setCustomUrl', to.path]);
+                        _paq.push(['setDocumentTitle', document.title]);
+                        _paq.push(['trackPageView']);
+                    }
                 });
                 app.use(router);
                 return router;
